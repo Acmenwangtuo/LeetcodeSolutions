b77bb326b030089bc75adb1645b54f5d5350675e
Acmenwangtuo <1198653145@qq.com>
2020-12-13 16:42:03 +0800

    'first'

---
+++ 5612.py
@@ -0,0 +1,12 @@
+from collections import  Counter
+from collections import defaultdict
+class Solution:
+    def boxDelivering(self, boxes: List[List[int]], portsCount: int, maxBoxes: int, maxWeight: int) -> int:
+        n = len(nums)
+        if n == k:
+            return 0
+
+
+if __name__ == '__main__':
+    c = Solution()
+    print(c.boxDelivering())
